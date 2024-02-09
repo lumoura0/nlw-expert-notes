c3a79f4fac0401346cd540a8b9a65f36402682b1
Luis Fernando <luisfernando.mourag@gmail.com>
2024-02-09 14:22:05 -0300

    feat: finishing the creation of the notes component

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -5,17 +5,22 @@ import { X } from "lucide-react";
 
 interface NoteCardProps {
   note: {
+    id: string,
     date: Date;
     content: string;
-  };
+  }
+  onNoteDeleted: (id: string) => void
 }
 
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
   return (
     <Dialog.Root>
       <Dialog.Trigger className=" focus-visible:ring-2 flex flex-col focus-visible:ring-lime-400 outline-none rounded-md text-left p-5 gap-3 bg-slate-800 overflow-hidden relative hover:ring-2 hover:ring-slate-600">
         <span className=" text-sm font-medium text-slate-300">
-          {note.date.toISOString()}
+          {formatDistanceToNow(note.date, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
         <p className=" text-sm leading-6 text-slate-400">{note.content}</p>
 
@@ -24,7 +29,7 @@ export function NoteCard({ note }: NoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className=" inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed left-1/2 top-1/2 max-w-[640px] h-[60vh] w-full overflow-hidden flex flex-col outline-none bg-slate-700 rounded-md -translate-y-1/2 -translate-x-1/2">
+        <Dialog.Content className="fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:max-w-[640px] md:h-[60vh] w-full overflow-hidden flex flex-col outline-none bg-slate-700 md:rounded-md md:-translate-y-1/2 md:-translate-x-1/2">
           <Dialog.Close className=" absolute top-0 right-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className=" size-5" />
           </Dialog.Close>
@@ -40,10 +45,10 @@ export function NoteCard({ note }: NoteCardProps) {
 
           <button
             type="button"
+            onClick={() => onNoteDeleted(note.id)}
             className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
           >
-            Deseja{" "}
-            <span className="text-red-400 group-hover:underline">
+            Deseja <span className="text-red-400 group-hover:underline">
               apagar essa nota
             </span>
             ?
